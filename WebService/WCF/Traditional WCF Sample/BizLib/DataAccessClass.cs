68c9c94cb24f8054fb914c320eac5197541e4c1a
ksyu33 <ksyu33@gmail.com>
2019-04-29 15:56:44 +0900

    Traditional WCF 예제 코드 추가

---
+++ WebService/WCF/Traditional WCF Sample/BizLib/DataAccessClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheOne.Transactions;
+
+namespace BizLib
+{
+    [FoxTransaction(FoxTransactionOption.Supported)]
+    class DataAccessClass : FoxDacBase
+    {
+        public DataSet GetAllProducts()
+        {
+            return this.DbAccess.ExecuteSqlDataSet("SELECT * FROM Products");
+        }
+    }
+}
---
+++ WebService/WCF/Traditional WCF Sample/WcfServiceWeb/OldWcfService.svc.cs
@@ -0,0 +1,26 @@
+﻿using BizLib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceWeb
+{
+    //
+    // WCF 서비스 클래스 구현
+    //
+    public class OldWcfService : IOldWcfService
+    {
+        public DataSet GetAllProducts()
+        {
+            // 비즈니스 로직 클래스를 호출한다.
+            using (var biz = new BizLogicClass())
+            {
+                return biz.GetAllProducts();
+            }
+        }
+    }
+}
