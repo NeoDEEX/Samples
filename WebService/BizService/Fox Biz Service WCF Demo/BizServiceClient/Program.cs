501cdf28cb952576eb667c6f7809fcd536193611
ksyu33 <ksyu33@gmail.com>
2019-05-08 15:09:14 +0900

    매뉴얼 용 예제인 Fox Biz Service Demo 예제 추가

---
+++ WebService/BizService/Fox Biz Service WCF Demo/BizServiceClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheOne.ServiceModel.Biz;
+
+namespace BizServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new FoxBizClient("BizService.svc"))
+            {
+                var request = new FoxBizRequest("데모비즈", "Echo");
+                // 매개변수를 설정 합니다.
+                request.Parameters["input"] = "Wow!";
+
+                var response = client.Execute(request);
+                Console.WriteLine($"Result: {response.Result}");
+            }
+        }
+    }
+}
---
+++ WebService/BizService/Fox Biz Service WCF Demo/BizServiceWeb/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace BizServiceWeb
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // 비즈 서비스를 구성합니다.
+            TheOne.ServiceModel.Biz.FoxBizServiceConfig.Configure();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
