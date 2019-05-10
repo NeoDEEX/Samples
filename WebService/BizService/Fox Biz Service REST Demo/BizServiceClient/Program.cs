48a0546223fdf4c73e8370b747f5e2a5cf4b65bc
ksyu33 <ksyu33@gmail.com>
2019-05-10 14:04:58 +0900

    Fox Biz Service REST API 메뉴얼 예제 코드 추가

---
+++ WebService/BizService/Fox Biz Service REST Demo/BizServiceClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using TheOne.ServiceModel.Biz;
+using TheOne.ServiceModel.Web;
+
+namespace BizServiceClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Request 객체 구성
+            var request = new FoxBizRequest("데모비즈", "Echo");
+            request.Parameters["input"] = "Wow!";
+
+            // 비즈 서비스(WCF) 호출
+            using (var client = new FoxBizClient("BizService.svc"))
+            {
+                var response = client.Execute(request);
+                Console.WriteLine($"Result: {response.Result}");
+            }
+
+            // 비즈 서비스 REST API 호출
+            using (var client = new FoxRestBizClient("api/bizservice"))
+            {
+                var response = client.Execute(request);
+                Console.WriteLine($"Result: {response.Result}");
+            }
+        }
+    }
+}
---
+++ WebService/BizService/Fox Biz Service REST Demo/BizServiceWeb/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.Http;
+
+namespace BizServiceWeb
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Web API 초기화
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            // 비즈 서비스를 구성합니다.
+            TheOne.ServiceModel.Biz.FoxBizServiceConfig.Configure();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
