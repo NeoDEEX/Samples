4df36e8836e8fd376f539380a56daa256b6006e4
ksyu33 <ksyu33@gmail.com>
2019-05-10 14:04:58 +0900

    Fox Biz Service REST API 메뉴얼 예제 코드 추가

---
+++ WebService/BizService/Fox Biz Service REST Demo/BizModule/BizClass.cs
@@ -0,0 +1,21 @@
+﻿using TheOne.ServiceModel.Biz;
+using TheOne.Transactions;
+
+namespace BizModule
+{
+    // 예제 비즈 클래스
+    // ClassId로 `데모비즈`를 사용합니다. ClassId를 명시하지 않은 경우,
+    // 네임스페이스를 포함하는 클래스의 전체 이름(BizModule.BizClass)이
+    // ClassId로 사용됩니다.
+    [FoxBizClass("데모비즈")]
+    public class BizClass : FoxBizBase
+    {
+        // 예제 비즈 메서드
+        // MethodId를 명시하지 않았기 때문에 메서드 이름이 MethodId로 사용됩니다.
+        [FoxBizMethod]
+        public string Echo(string input)
+        {
+            return $"Hello Fox Biz Service: {input}";
+        }
+    }
+}
---
+++ WebService/BizService/Fox Biz Service REST Demo/BizServiceWeb/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BizServiceWeb
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            // Fox Biz Service REST API 라우팅 설정
+            config.Routes.MapHttpRoute(
+                name: "FoxBizServiceRoute",
+                routeTemplate: "api/bizservice/{action}",
+                defaults: new { controller = "FoxBizService" }
+            );
+        }
+    }
+}
