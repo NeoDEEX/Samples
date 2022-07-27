6e6f99b2b1b0118e5d1d2ecd75f29881d16f0a4c
유수석 <ksyu33@gmail.com>
2022-07-27 13:28:25 +0900

    Fox Query 매크로 예제 추가.

---
+++ Data/FoxQuery/Dynamic Query/DataServiceWebApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DataServiceWebApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            TheOne.ServiceModel.Biz.FoxBizServiceConfig.Configure();
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "FoxDataService",
+                routeTemplate: "api/dataservice/{action}",
+                defaults: new { controller = "FoxDataService" }
+            );
+        }
+    }
+}
---
+++ Data/FoxQuery/Dynamic Query/SimpleMacro/DacComp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleMacro
+{
+    internal class DacComp : DacBase
+    {
+        public DataSet GetData(int? categoryId)
+        {
+            var parameters = new { CategoryId = categoryId };
+            return this.DbAccess.ExecuteQueryDataSet("Query.GetData", parameters);
+        }
+    }
+}
