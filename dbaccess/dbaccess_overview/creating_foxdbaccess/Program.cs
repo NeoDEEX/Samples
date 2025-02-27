fc651fd9752fc55ab485fe774e37b26594373f7a
유수석 <ksyu33@gmail.com>
2025-02-27 12:25:57 +0900

    Fox DB Access 의 Overview 예제 추가.

---
+++ dbaccess/dbaccess_overview/creating_foxdbaccess/Program.cs
@@ -0,0 +1,69 @@
+﻿using NeoDEEX.Data;
+using NeoDEEX.Data.NpgsqlClient;
+using NeoDEEX.Data.OracleClient;
+using Npgsql;
+using NpgsqlTypes;
+using System.Data;
+using System.Diagnostics;
+
+namespace creating_foxdbaccess;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //CreateConcreteClass();
+        //UsingFactoryPattern();
+        CreatingSpecificDbAccess();
+    }
+
+    static readonly string npgsqlConnectionString = "Server=test-prostgresql.postgres.database.azure.com;Database=testdb;Port=5432;User Id=tester;Password=test;Ssl Mode=Require;";
+
+    static void CreateConcreteClass()
+    {
+        FoxNpgsqlDbAccess dbAccess = new(npgsqlConnectionString);
+        NpgsqlParameter[] parameters =
+        [
+            new NpgsqlParameter("product_id", 20),
+            new NpgsqlParameter("product_name", "A%")
+        ];
+        string query = "SELECT * FROM products WHERE product_id <= :product_id AND product_name LIKE :product_name";
+        DataSet ds = dbAccess.ExecuteSqlDataSet(query, parameters);
+        foreach(DataRow row in ds.Tables[0].Rows)
+        {
+            Console.WriteLine($"Product ID: {row["product_id"]}, Product Name: {row["product_name"]}");
+        }
+    }
+
+    static void UsingFactoryPattern()
+    {
+        FoxDbAccess dbAccess = FoxDbAccess.CreateDbAccess();
+        Debug.Assert(dbAccess is FoxNpgsqlDbAccess);
+        FoxDbParameterCollection parameters = dbAccess.CreateParamCollection();
+        Debug.Assert(parameters is FoxNpgsqlParameterCollection);
+        parameters.AddWithValue("product_id", DbType.Int16, 20);
+        parameters.AddWithValue("product_name", DbType.String, "A%");
+        string query = "SELECT * FROM products WHERE product_id <= :product_id AND product_name LIKE :product_name";
+        DataSet ds = dbAccess.ExecuteSqlDataSet(query, parameters);
+        foreach (DataRow row in ds.Tables[0].Rows)
+        {
+            Console.WriteLine($"Product ID: {row["product_id"]}, Product Name: {row["product_name"]}");
+        }
+    }
+
+    static void CreatingSpecificDbAccess()
+    {
+        FoxDbAccess dbAccess = FoxDbAccess.CreateDbAccess("Oracle");
+        Debug.Assert(dbAccess is FoxOracleDbAccess);
+        FoxDbParameterCollection parameters = dbAccess.CreateParamCollection();
+        Debug.Assert(parameters is FoxOracleParameterCollection);
+        parameters.AddWithValue("product_id", DbType.Int16, 20);
+        parameters.AddWithValue("product_name", DbType.String, "A%");
+        string query = "SELECT * FROM products WHERE product_id <= :product_id AND product_name LIKE :product_name";
+        DataSet ds = dbAccess.ExecuteSqlDataSet(query, parameters);
+        foreach (DataRow row in ds.Tables[0].Rows)
+        {
+            Console.WriteLine($"Product ID: {row["product_id"]}, Product Name: {row["product_name"]}");
+        }
+    }
+}
---
+++ dbaccess/dbaccess_overview/legacy_dbaccess/Program.cs
@@ -0,0 +1,25 @@
+﻿using Npgsql;
+
+namespace legacy_dbaccess;
+
+internal class Program
+{
+    static readonly string ConnectionString = "Server=test-prostgresql.postgres.database.azure.com;Database=testdb;Port=5432;User Id=tester;Password=test;Ssl Mode=Require;";
+
+    static void Main(string[] args)
+    {
+        string query = "SELECT COUNT(*) FROM products";
+        using NpgsqlConnection conn = new(ConnectionString);
+        using NpgsqlCommand cmd = new(query, conn);
+        conn.Open();
+        try
+        {
+            long count = (long)cmd.ExecuteScalar()!;
+            Console.WriteLine($"Number of products: {count}");
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+}
