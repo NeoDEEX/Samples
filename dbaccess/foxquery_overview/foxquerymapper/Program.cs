43ed22b397f96a8814f0f3debe09af2138692ef8
유수석 <ksyu33@gmail.com>
2025-06-24 11:59:25 +0900

    FoxQueryMapper 관련 예제 추가.

---
+++ dbaccess/foxquery_overview/foxquerymapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using NeoDEEX.Data;
+using System.Data;
+
+namespace foxquerymapper;
+
+internal class Program
+{
+    static void Main()
+    {
+        // 연결 문자열 노출을 막기 위해 user-secrets 에서 연결 문자열을 읽어 재설정합니다.
+        IConfigurationRoot config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+        FoxDatabaseConfig.ExternalConfiguration = config;
+
+        ExecutingSimpleFoxQuery();
+    }
+
+    static void ExecutingSimpleFoxQuery()
+    {
+        FoxDbAccess dbAccess = FoxDbAccess.CreateDbAccess();
+        string queryId = "northwind:get_product";
+        var parameters = new { product_id = 3 };
+        DataSet ds = dbAccess.ExecuteQueryDataSet(queryId, parameters);
+        foreach (DataRow row in ds.Tables[0].Rows)
+        {
+            Console.WriteLine($"Product ID: {row["product_id"]}, Product Name: {row["product_name"]}");
+        }
+    }
+}
---
+++ dbaccess/foxquery_overview/foxquerymapper/README.md
@@ -0,0 +1,7 @@
+﻿# FoxQueryMapper 예제
+
+이 예제는 Fox DB Access 에 포함된 FoxQueryMapper 와 관련된 예제 코드 입니다.
+
+상세한 내용은 [FoxQueryMapper 문서](https://neodeex.github.io/doc/dbaccess/foxquery/foxquerymapper/)를 참고 하십시요.
+
+---
